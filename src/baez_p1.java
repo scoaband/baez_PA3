b02a66045f16743856523737644080378a825dc4
Johnathan Baez <scoaband@hotmail.com>
2019-10-16 23:41:11 -0400

    upDate

--- src/baez_p1.java
+++ src/baez_p1.java
@@ -9,9 +9,7 @@
 
     9. Program shall display the number of correct and incorrect responses at the end of a session
 
-    11. Program shall ask the user if they wish to begin a new session once the current session has terminated
-
-    17. 5 is a mix of addition, subtraction, multiplication, and division.)
+    17. choose 5 is a mix of addition, subtraction, multiplication, and division.)
 */
 
 
@@ -20,36 +18,31 @@ import java.util.Random;
 import java.util.Scanner;
 public class baez_p1 {
 
-    private static final int ADD = 1;
-    private static final int SUB = 2;
-    private static final int MULT = 3;
-    private static final int DIV = 4;
-    private static final int RANDTYPE = 5;
-
     private static int Lv = 0;
 
-    private static int Reset(){
-        int reset = 1;
 
-        return reset;
+    private static int Reset(){
+
+        return 1;
     }
 
     private static int setLevel(int n){
 
-        int Lv = n;
-
+        Lv = n;
         return Lv;
     }
 
     private static void settings(){
         Scanner scan = new Scanner(System.in);
-        int problemType = 0;
-        int level = 0;
+        int probType;
+        int level;
 
         System.out.println(" Welcome to Computer-Assisted Instruction (CAI)");
         do{
-            System.out.println("1 = 1 digit, 2 = 2 digit, 3 = 3 digit, 4 = 4 digit\n");
-            System.out.print(" Choose your Level 1 - 4: ");
+
+            System.out.println("| 1 = 1 digit\n| 2 = 2 digit\n| 3 = 3 digit\n| 4 = 4 digit");
+            System.out.print(" Choose difficulty  Level 1 - 4: ");
+
             level = scan.nextInt();
             if (level >= 5 || level <= 0){
                 System.out.println("Error number: " + level +", Levels are from 1 to 4, Try again!\n");
@@ -57,21 +50,25 @@ public class baez_p1 {
 
         }while(level >= 5 || level <= 0);
         do{
-            System.out.print(" 1 = Addition | 2 = Subtraction | 3 = Multiplication | 4 = Division | 5  = Random Type |\n" +
-                    " Choose Problem Type: ");
-            problemType = scan.nextInt();
-            if (problemType >= 6 || problemType <= 0){
-                System.out.println("Error number: " + problemType +", problem Type are from 1 to 5, Try again!\n");
+            System.out.print("| 1 = Addition \n| 2 = Subtraction \n| 3 = Multiplication \n| 4 = Division \n" +
+                    "| 5  = Random Type \n" +
+                    "| Choose Problem Type: ");
+            probType = scan.nextInt();
+            if (probType >= 6 || probType <= 0){
+                System.out.println("Error number: " + probType +", problem Type are from 1 to 5, Try again!\n");
             }
 
-        }while(problemType >= 6 || problemType <= 0);
+        }while(probType >= 6 || probType <= 0);
 
         System.out.print("______________________________________________________________________\n");
-
-
+        String ant =  problemType(probType);
         Lv = setLevel(level);
+        startApp(level, ant, scan);
+    }
 
-        startApp(level, problemType, scan);
+    private static int getRanNum(int n){
+        SecureRandom rand = new SecureRandom();
+        return rand.nextInt(n);
     }
 
     private static int  genRNum(){
@@ -94,43 +91,147 @@ public class baez_p1 {
                 break;
         }
 
-        int genRndNum = rand.nextInt(seed);
-
-        return genRndNum;
+        return rand.nextInt(seed);
     }
 
-    public static double  genQuest(){
+    private static void genQuest(int n1, int n2, String pt){
 
-        return 0;
+        System.out.print(" How much is " + n1 + " " + pt + " " + n2 + "?\n");
     }
 
-    private static int problemType(int problemType){
+    private static String problemType(int n){
 
-        switch (problemType){
+        String PrTy = " ";
+
+        switch (n){
             case 1:
-                problemType = ADD;
+                PrTy = "ADD";
                 break;
             case 2:
-                problemType = SUB;
+                PrTy = "SUB";
                 break;
             case 3:
-                problemType = MULT;
+                PrTy = "MULT";
                 break;
             case 4:
-                problemType = DIV;
+                PrTy = "DIV";
                 break;
-            default:
-                problemType = RANDTYPE;
+            case 5:
+                PrTy = "RANDTYPE";
                 break;
         }
-
-
-        return problemType;
+        return PrTy;
     }
 
 
+    private static void  startApp(int level, String maths, Scanner scan){
+        int x1;
+        int x2;
+        int reset;
+        int answer;
+        int correctAns = 0;
+        int grade = 0;
 
-    private static int genResponse(int correctAns, int answer){
+
+        do{
+
+            for (int i = 0; i < 10; ++i) {
+                String proType = ":)";
+                x1 = genRNum();
+                x2 = genRNum();
+
+                switch (maths) {
+                    case "Random":
+                        int x3 = getRanNum(3);
+                        System.out.println(x3);
+                        switch (x3){
+                            case 0:
+                                proType = "+";
+                                correctAns = (x1 + x2);
+                                break;
+                            case 1:
+                                proType = "-";
+                                correctAns = (x1 - x2);
+                                break;
+                            case 2:
+                                proType = "*";
+                                correctAns = (x1 * x2);
+                                break;
+                            case 3:
+                                proType = "/";
+                                if (x2 < 1 || x1 != x2) {
+                                    correctAns = (x2 / x1);
+                                } else {
+                                    correctAns = (x1 / x2);
+                                }
+                                break;
+                        }
+                        break;
+                    case "ADD":
+                        proType = "+";
+                        correctAns = (x1 + x2);
+                        break;
+                    case "SUB":
+                        proType = "-";
+                        correctAns = (x1 - x2);
+                        break;
+                    case "MULT":
+                        proType = "*";
+                        correctAns = (x1 * x2);
+                        break;
+                    case "DIV":
+                        proType = "/";
+                        if (x2 < 1 || x1 != x2) {
+                            correctAns = (x2 / x1);
+                        } else {
+                            correctAns = (x1 / x2);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                System.out.print("\n| Level: " + setLevel(level) + "  |  Problem Type: " + maths);
+                System.out.print(" | ");
+
+                System.out.print("Question " + (i + 1) + " / 10 " );
+
+                System.out.println();
+                genQuest(x1, x2, proType);
+                System.out.print(" Your answer:  ");
+                answer = scan.nextInt();
+                genResponse(correctAns, answer);
+                System.out.println();
+                if (answer == correctAns){
+                    ++grade;
+                }
+
+            }
+            reset = Reset();
+        }while(reset != 1);
+
+        int newGrade = grade * 10;
+
+        if (newGrade >= 75){
+
+            System.out.println("\n Score: " +newGrade+ "%\n" +
+                    " Congratulations, you are ready to go to the next level!");
+        }else {
+            System.out.println(" Please ask your teacher for extra help. \n");
+        }
+            System.out.print(" Press 1 to begin a new session: ");
+        int newSession = scan.nextInt();
+        if (newSession == 1) {
+            System.out.print("\n\n");
+            settings();
+        } else {
+            System.out.println("Good bye!");
+        }
+
+        scan.close();
+    }
+
+    private static void genResponse(int correctAns, int answer){
         Random rand = new Random();
         int count;
         count = rand.nextInt(4);
@@ -173,86 +274,6 @@ public class baez_p1 {
 
             System.out.print("_._._._._._._._._._._._._._._._._._._._._._._._._._");
         }
-        return answer;
-    }
-
-    private static void  startApp(int level, int problemType, Scanner scan){
-        int x1 = 0;
-        int x2 = 0;
-        int reset = 0;
-        int answer = 0;
-        int correctAns = 0;
-        int grade = 0;
-
-        do{
-
-            for (int i = 0; i < 10; ++i) {
-                String proType = " ";
-                x1 = genRNum();
-                x2 = genRNum();
-
-                System.out.print("\n| Level: " + setLevel(level) + "  |  Problem Type: " + problemType);
-                System.out.print(" | ");
-
-                System.out.print("Question " + (i + 1) + " / 10 " );
-
-                switch (problemType) {
-                    case 1:
-                        proType = " plus";
-                        correctAns = (x1 + x2);
-                        break;
-                    case 2:
-                        proType = "minus";
-                        correctAns = (x1 - x2);
-                        break;
-                    case 3:
-                        proType = "times";
-                        correctAns = (x1 * x2);
-                        break;
-                    case 4:
-                        proType = "quotient";
-                        if (x2 < 1 || x1 != x2) {
-                            correctAns = (x2 / x1);
-                        } else {
-                            correctAns = (x1 / x2);
-                        }
-                        break;
-                }
-
-                System.out.println();
-                System.out.print(" How much is " + x1 + " " + proType + " " + x2 + "?\n");
-                System.out.print(" Your answer:  ");
-                answer = scan.nextInt();
-                genResponse(correctAns, answer);
-                System.out.println();
-                if (answer == correctAns){
-                    ++grade;
-                }
-
-            }
-            reset = Reset();
-        }while(reset != 1);
-
-        int newGrade = grade * 10;
-
-        if (newGrade >= 75){
-
-            System.out.println("\n Scoare: " +newGrade+ "%\n Congratulations, you are ready to go to the next level!");
-        }else {
-            System.out.println(" Please ask your teacher for extra help. \n");
-        }
-            System.out.print(" Press 1 to begin a new session: ");
-        int newSession = scan.nextInt();
-        switch (newSession){
-            case 1:
-                System.out.print("\n\n");
-                settings();
-                break;
-            default:
-                System.out.println("Good bye!");
-        }
-
-        scan.close();
     }
 
     public static void main(String[] args) {
